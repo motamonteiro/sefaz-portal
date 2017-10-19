0598b5232add706715e233c2db2507a648c5c949
Alexandre Mota Monteiro <motamonteiro@gmail.com>
2017-10-19 10:37:16 -0200

    Adicionando versao do sistema na View e no PortalHelper e atualizando o motamonteiro/helpers no composer.json

--- Views/layout/includes/header-barra-sistema.blade.php
+++ Views/layout/includes/header-barra-sistema.blade.php
@@ -1,12 +1,12 @@
 <img src="{{ $portal->nmeIconeSistemaAtual }}" alt="" width="37" class="pull-left">
 @if(config('sistema.modulo.codigo') != '')
     <p class="sigla-sistema">{{ $portal->codSistemaAtual .' - '.$portal->nmeSistemaAtual }}</p>
-    <p class="nome-sistema">{{ $portal->nmeModuloAtual }}</p>
+    <p class="nome-sistema">{{ $portal->nmeModuloAtual }} <span>{{ $portal->codVersaoSistemaAtual }}</span></p>
 @else
     @if($portal->codModuloAtual)
         <p class="sigla-sistema">{{ $portal->codModuloAtual .' - '.$portal->nmeModuloAtual }}</p>
-        <p class="nome-sistema">{{ $portal->nmeSistemaAtual }}</p>
+        <p class="nome-sistema">{{ $portal->nmeSistemaAtual }} <span>{{ $portal->codVersaoSistemaAtual }}</span></p>
     @else
-        <p class="sigla-sistema">{{ $portal->nmeSistemaAtual .' - '.$portal->dscSistemaAtual }}</p>
+        <p class="sigla-sistema">{{ $portal->nmeSistemaAtual .' - '.$portal->dscSistemaAtual }} <span>{{ $portal->codVersaoSistemaAtual }}</span></p>
     @endif
 @endif
