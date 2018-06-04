f3abb7dcfd76ffab8893f252211ae8cfa3397136
Alexandre Mota Monteiro <motamonteiro@gmail.com>
2018-06-04 16:42:00 -0300

    Tratando mensagens de bloqueio de firewall

--- Helpers/ApiHelper.php
+++ Helpers/ApiHelper.php
@@ -64,9 +64,21 @@ class ApiHelper extends BaseApiHelper
                 $message = 'Erro ao acessar o servidor da api.';
 
             } else {
+                /**
+                 * Se for um erro de FIREWALL da SEFAZ
+                 * Exemplo
+                 *  $error['messages'][0]: server_error_converting_to_json
+                 *  $error['messages'][1]: <html><head><title>Request Rejected</title></head><body>The requested URL was rejected. Please consult with your administrator.<br><br>Your support ID is: 9999999999999999999<br><br><a href='javascript:history.back();'>[Go Back]</a></body></html>
+                 */
+                if ($error['messages'][0] == 'server_error_converting_to_json' && strpos($error['messages'][1], '<title>Request Rejected</title>') !== false) {
+                    $supportId = explode('<br><br>', $error['messages'][1]);
+                    $supportId = explode(':', $supportId[1] ?? '')[1] ?? '';
+                    $message = 'O Firewall da SEFAZ está bloqueando algum recurso desse sistema. Favor abrir um chamado no CSS (Central de Solicitações da SEFAZ) com a informação abaixo:<br> <strong>Bloqueio de firewall: '.$supportId.'</strong>';
+                } else {
+                    $message = 'Erro ao acessar a resposta da api.';
+                }
 
                 event(new ServidorApiNaoRespondeuCorretamenteEvent($this,$error));
-                $message = 'Erro ao acessar a resposta da api.';
             }
 
             return [self::ERROR => 'true', self::STATUS_CODE => $error[self::STATUS_CODE], self::MESSAGES => $message];
