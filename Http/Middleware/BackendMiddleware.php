49ab9b2269853e6e0f3761ced06229355e92dde0
Alexandre Mota Monteiro <motamonteiro@gmail.com>
2019-07-03 15:51:03 -0300

    Adiciona parametro modulo no BackendMiddleware

--- Http/Middleware/BackendMiddleware.php
+++ Http/Middleware/BackendMiddleware.php
@@ -11,29 +11,32 @@ class BackendMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @param string $funcao
+     * @param string $modulo
      * @return mixed
      */
-    public function handle($request, Closure $next, $funcao)
+    public function handle($request, Closure $next, $funcao, $modulo = '')
     {
+        $modulo = ($modulo == '') ? config('sistema.modulo.codigo') : $modulo;
         $portal = new PortalHelper(10);
-        $portal->usuarioLogado->getUsuarioLogado(config('sistema.codigo'), config('sistema.modulo.codigo'));
+        $portal->usuarioLogado->getUsuarioLogado(config('sistema.codigo'), $modulo);
 
         if (!$portal->usuarioLogado->validarUsuarioLogado()) {
             $statusCode = ($portal->usuarioLogado->statusCode != '') ? $portal->usuarioLogado->statusCode : '200';
-            $msgErro = ($portal->usuarioLogado->msgErro != '') ? $portal->usuarioLogado->msgErro : config('sistema.portal_api.token_key').'_invalid_user';
+            $msgErro = ($portal->usuarioLogado->msgErro != '') ? $portal->usuarioLogado->msgErro : config('sistema.portal_api.token_key') . '_invalid_user';
             $resposta = ['error' => true, 'message' => $msgErro];
             return response()->json($resposta, $statusCode);
         }
 
         if (empty($portal->usuarioLogado->permissoesPortal)) {
-            $resposta = ['error' => true, 'message' => 'O usuário não tem permissão no módulo '.config('sistema.codigo') .' - '. config('sistema.modulo.codigo')];
+            $resposta = ['error' => true, 'message' => 'O usuário não tem permissão no módulo ' . config('sistema.codigo') . ' - ' . $modulo];
             return response()->json($resposta, 401);
         }
 
-        if (!$portal->usuarioLogado->helper->checarValorArrayMultidimensional('codFuncao', $funcao, $portal->usuarioLogado->permissoesPortal )) {
-            $resposta = ['error' => true, 'message' => 'O usuário não tem permissão na função '.$funcao];
+        if (!$portal->usuarioLogado->helper->checarValorArrayMultidimensional('codFuncao', $funcao, $portal->usuarioLogado->permissoesPortal)) {
+            $resposta = ['error' => true, 'message' => 'O usuário não tem permissão na função ' . $funcao . ' do módulo ' . $modulo];
             return response()->json($resposta, 401);
         }
 
